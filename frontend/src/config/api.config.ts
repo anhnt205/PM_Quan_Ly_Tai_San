2b0be41a375dcaed23047e4e04a4ab97f13b2184
davu111 <vuvanvuonglqm@gmail.com>
2026-07-09 16:23:57 +0700

    fix: change backend api

--- frontend/src/config/api.config.ts
+++ frontend/src/config/api.config.ts
@@ -3,7 +3,7 @@ import { store } from "../redux/store";
 import { logout } from "../redux/userSlice";
 import { ROUTES } from "../utils/routes";
 
-const API_URL = import.meta.env.VITE_BASE_API || "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_KONG_API || "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: API_URL,
